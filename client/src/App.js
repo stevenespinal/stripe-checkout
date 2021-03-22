c7efb5ec44e06df5f6a5818f65fe3ee7f2ccfeb7
Steven Espinal <stevenjesusespinal@gmail.com>
2021-03-22 19:48:40 -0400

    Initial working files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import Product from "./pages/Product";
 
 function App() {
-  return <div>App</div>;
+    return (
+        <Router>
+            <Switch>
+                <Route component={Home} exact path="/"/>
+                <Route component={Result} exact path="/result"/>
+                <Route component={Product} exact path="/:id"/>
+
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
